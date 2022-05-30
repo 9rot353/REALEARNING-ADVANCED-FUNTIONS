686a68bb82e4d75e1869e3c64bc34ba74e3fd268
Saivon Schenck <schencksaivon@Saivons-Air.hsd1.ga.comcast.net>
2022-05-30 15:47:55 -0400

    map, some, every-assignment completed

--- find-filter-map-some-every.js
+++ find-filter-map-some-every.js
@@ -49,3 +49,61 @@ function parksFiltered (parks) {
 }
 console.log( parksFiltered(parks))
 
+// Practice 5) Practice the map method
+
+let mappedParks = parks.map((park) => park.name)
+console.log(mappedParks)
+
+// Practice 6) Using the same data create a function that return only the park names without using
+// the map method,
+
+function mapParks (parks) {
+let result = []
+
+    for (let i = 0; i < parks.length; i++) {
+        result.push(parks[i].name)
+    }
+
+return result
+}
+
+console.log(mapParks(parks ))
+
+// Practice 7) Practice using the some method
+
+let ratingsCheck = parks.some((park) => park.rating > 8)
+console.log(ratingsCheck)
+
+//Practice 8) Check if there are rating below 5 without using the some method.
+
+function belowFive (parks) {
+
+    for (let i = 0; i < parks.length; i++) {
+        return (parks[i].rating > 3) 
+    }
+}
+
+console.log(belowFive(parks))
+
+// Practice 9)  Practice the every method
+
+let checkEveryParkRating = parks.every((park) => park.rating > 2)
+console.log(checkEveryParkRating)
+
+//Practice 10) check if every rating is about 2 without using the every method
+
+function EveryRatingGreater (parks,rating) {
+    let truthHolder = false;
+
+    for(let i = 0; i < parks.length; i++) {
+    let parkRating = parks[i].rating;
+        if (parkRating > rating) truthHolder = true;
+}
+return truthHolder;
+}
+
+console.log(EveryRatingGreater(parks, 5))
+
+let symbol = "<";
+
+console.log(symbol)
